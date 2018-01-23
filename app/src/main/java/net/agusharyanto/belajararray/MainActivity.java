7bdbdfec4d96c30d72a26f915af8d5710ae19169
jatisari <jatisari.innovation@gmail.com>
2018-01-23 13:29:35 +0700

    Spinner Array

--- app/src/main/java/net/agusharyanto/belajararray/MainActivity.java
+++ app/src/main/java/net/agusharyanto/belajararray/MainActivity.java
@@ -1,13 +1,19 @@
 package net.agusharyanto.belajararray;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String[] fruits = {"Mango","Banana","Guava"};
-    int[] values = {15000, 20000, 10000, 40000};
+    int[] prices = {15000, 20000, 10000};
     TextView textViewFruit;
+    Spinner spinnerFruit;
     String nama = "Zainal Arifin";
     int jumlah = 100;
     @Override
@@ -15,8 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewFruit = (TextView) findViewById(R.id.textViewFruit);
-       // printFruit();
-        printNama();
+        spinnerFruit = (Spinner) findViewById(R.id.spinnerFruit);
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, fruits);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinnerFruit.setAdapter(spinnerArrayAdapter);
+        spinnerFruit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplication(), "position :"+position+" id:"+id,Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
+        printFruit();
+        //printNama();
     }
 
     private void printNama() {
@@ -31,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     private void printFruit() {
         String buah="";
         for (int i=0; i<fruits.length; i++){
-            buah = buah+fruits[i]+"\n";
+            buah = buah+fruits[i] +"-"+prices[i]+"\n";
         }
         textViewFruit.setText(buah);
     }
